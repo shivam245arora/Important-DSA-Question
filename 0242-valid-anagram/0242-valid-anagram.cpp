20489f334448261e4e33311ee10b7540ac0a5ccf
Shivam Arora <119662160+shivam245arora@users.noreply.github.com>
2026-05-13 22:23:04 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.89%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string a, string b) {
+        if (a.length() != b.length())
+    {
+        return false;
+    }
+    int count[26] = {0};
+    for (char c : a)
+    {
+        count[c - 'a']++;
+    }
+    for (char c : b)
+    {
+        count[c - 'a']--;
+    }
+    for(int i=0;i<26;i++)
+    {
+        if (count[i] != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+    }
+};
