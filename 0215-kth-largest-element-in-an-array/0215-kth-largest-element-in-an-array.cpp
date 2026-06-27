b58aba6d997231780df8f011c3f92d3238fdeb4e
Shivam Arora <119662160+shivam245arora@users.noreply.github.com>
2026-06-27 15:40:04 +0530

    Time: 39 ms (37.67%), Space: 72.5 MB (25.3%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -13,7 +13,6 @@ public:
          priority_queue<int,vector<int>,greater<int>> pq;
         for (int i = 0; i < nums.size(); i++) {
             pq.push(nums[i]);
-        
         if(pq.size()>k) pq.pop();
         }
         return pq.top();
