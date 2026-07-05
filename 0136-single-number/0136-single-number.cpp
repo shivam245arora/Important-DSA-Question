2afaede34c8c3aa086f66a642e1c49aeb65641dc
Shivam Arora <119662160+shivam245arora@users.noreply.github.com>
2026-07-05 19:28:01 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.01%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int num : nums){
-            mp[num]++;
+        //     unordered_map<int,int>mp;
+        //     for(int num : nums){
+        //         mp[num]++;
+        //     }
+        //     for(auto& pair: mp){
+        //         if(pair.second==1){
+        //             return pair.first;
+        //         }
+        //     }
+        //    return -1;
+
+        int ans = 0;
+        for (int n : nums) {
+            ans ^= n;
         }
-        for(auto& pair: mp){
-            if(pair.second==1){
-                return pair.first;
-            }
-        }
-       return -1; 
+        return ans;
     }
-};
+}
+
+;
